ad0b72b005327d0794a4c47128059d38cad327dc
EntornoJT <vkurylo417@gmail.com>
2023-07-03 10:28:17 +0100

    address changed

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from langchain.document_loaders import S3FileLoader
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
+import os
 
 # import environment variable
 load_dotenv()
@@ -15,7 +16,7 @@ app.add_middleware(CORSMiddleware, allow_origins=[
                    "*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
 # read-file api
-# @param
+# GET: @param
 # bucket: bucket name
 # file: file id
 
@@ -25,8 +26,8 @@ async def read_file(bucket: str = "langchaintest", file: str = "test.pdf"):
     try:
         loader = S3FileLoader(bucket, file)
         return loader.load()
-    except:
-        raise HTTPException(status_code=500, detail="Error occured")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=e)
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8080)
+    uvicorn.run("app:app", host="127.0.0.1", port=8000)
